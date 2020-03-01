c9de63b01cde74f72b49d02fb395a81e390f6d95
Imran Sayed <imranhsayed@gmail.com>
2020-03-01 16:00:41 +0530

    Update

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 class Game {
 	constructor() {
+		document.body.click();
 		this.init();
 	}
 
@@ -7,53 +8,33 @@ class Game {
 		this.container = document.getElementById( 'container' );
 		this.scoreEl = document.getElementById( 'score' );
 		this.missedScoreEl = document.getElementById( 'missed-score' );
-		this.shootingAudio = document.getElementById( 'shooting-audio' );
-		this.backgroundMusic = document.getElementById( 'background-music' );
-		this.missedAudio = document.getElementById( 'lost-audio' );
+		this.shootingAudio = new Audio('./sound/shooting.mp3');
+		this.missedAudio = new Audio('./sound/missed.mp3');
+		this.backgroundMusic = new Audio('./sound/game-background.mp3');
 		this.containerHeight = this.container.offsetHeight;
 		this.score = 0;
-		this.ballCount = 5;
-		this.dropBallSpeed = 5;
 		this.missedCount = 0;
+		this.ballCount = 30;
+		this.dropBallSpeed = 1;
 
 		this.backgroundMusic.play();
 
-		for( let i = 0; i < this.ballCount; i++ ) {
+		for( let i = 0; i < this.ballCount; i++  ) {
+
 			const ballEl = this.createBall();
 			const leftPos = this.getRandomNo( 1000 );
 			const endPos = this.containerHeight + 50;
-			let interVal = this.getRandomNo( 100 ) + ( i * 1000);
+			let intervalTime = this.getRandomNo( 100 ) + ( i * 1000 );
 
-			setTimeout( () => {
+
+			const interval = setInterval( () => {
 				this.dropBall( ballEl, leftPos, endPos, this.dropBallSpeed );
-			}, interVal );
-		}
-	}
-
-	dropBall( ballEl, leftPos, endPos, speed ) {
-		let currentTop = 0;
-
-		// Set left position.
-		ballEl.style.left = leftPos + 'px';
-		ballEl.style.backgroundColor = '#' + this.getRandomNo( 1000 );
-
-		const interval = setInterval( () => {
-
-			if ( endPos === currentTop ) {
 				clearInterval( interval );
-				ballEl.remove();
-				this.missedCount += 1;
-				this.missedScoreEl.textContent = this.missedCount;
-				this.missedAudio.play();
+			}, intervalTime )
 
-			} else {
-				currentTop++;
-				ballEl.style.top = currentTop + 'px';
-			}
+		}
 
-		}, speed );
 
-		ballEl.setAttribute( 'data-interval-id', interval );
 	}
 
 	createBall() {
@@ -68,33 +49,56 @@ class Game {
 		this.container.appendChild( ballEl );
 
 		return ballEl;
+
 	}
 
-	/**
-	 * Get random number upto the given range.
-	 *
-	 * @param {int} range Range ( e.g. for 0-99, range will be 100, for 0-999, range will be 1000 )
-	 * @return {number}
-	 */
-	getRandomNo( range ) {
-		return Math.floor(Math.random() * range);
+	dropBall( ballEl, leftPos, endPos, speed ) {
+		let currentTop = 0;
+
+		ballEl.style.left = leftPos + 'px';
+		ballEl.style.backgroundColor = '#' + this.getRandomNo( 1000 );
+
+		const interval = setInterval( () => {
+			if ( endPos === currentTop ) {
+				clearInterval( interval );
+				this.missedAudio.play();
+				ballEl.remove();
+				this.missedCount += 1;
+				this.missedScoreEl.textContent = this.missedCount.toString();
+
+			} else {
+				currentTop++;
+				ballEl.style.top = currentTop + 'px';
+			}
+		}, speed );
+
+		ballEl.setAttribute( 'data-interval-id', interval );
+
 	}
+
+
 
 	shootBall( event ) {
-		const points = event.target.getAttribute( 'data-points' );
+
 		const targetEl = event.target;
-		const intervalId = parseInt( targetEl.getAttribute( 'data-interval-id' ) ) ;
+		const points = targetEl.getAttribute( 'data-points' );
+		const intervalId = parseInt( targetEl.getAttribute( 'data-interval-id' ) );
 
 		clearInterval( intervalId );
 
-		this.shootingAudio.play();
 		this.addScore( points );
+		this.shootingAudio.play();
+
 		targetEl.remove();
 	}
 
 	addScore( points ) {
 		this.score += parseInt( points );
 		this.scoreEl.textContent = this.score.toString();
+	}
+
+	getRandomNo( range ) {
+		return Math.floor( Math.random() * range )
 	}
 }
 
